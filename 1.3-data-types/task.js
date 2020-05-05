81b89df5d21f7ffa492220d4885a25d1294dd91f
Svetlana Gorkovenko <lipa-@mail.ru>
2020-05-05 23:39:13 +0300

    First commit 1.3 data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,24 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    "use strict";
+
+    let credit = amount - contribution;
+    let monthNumber = (date.getFullYear() - new Date().getFullYear()) * 12;
+    let P = (1 / 12) * (percent / 100);
+    let payment = credit * (P + P / (Math.pow(1 + P, monthNumber) - 1));
+    let totalAmount = payment * monthNumber;
+
+    console.log(totalAmount.toFixed(2));
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    
+    if (typeof name === "undefined" || "null" || "") {
+        console.log(`Привет, мир! Меня зовут Аноним`);
+        return `Привет, мир! Меня зовут Аноним`;
+    } else if (typeof name === String) {
+        console.log(`Привет, мир! Меня зовут ${name}`);
+        return `Привет, мир! Меня зовут ${name}`;
+    }
+    
 }
