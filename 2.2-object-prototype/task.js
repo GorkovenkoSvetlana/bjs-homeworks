fb9063ce0246e3c0da929712deea5fbbe96c7346
Svetlana Gorkovenko <lipa-@mail.ru>
2020-05-18 12:13:50 +0300

    First commit ДЗ 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,34 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
-}
+   let animalSound = animal.sound;
+   if (animal === undefined) {
+       return null
+   };
+       return animalSound
+   }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks.length === 0) {
+        return 0
+    };
+    let average = 0;
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    average = sum / marks.length;
+    return Math.round(average); 
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
-}
+    let now = Date.now();
+    birthday = Number(new Date(birthday));
+    let diff = now - birthday;
+    let age = diff / 31557600000; 
+    // 31557600000 как среднее арифметическое за 4 года, три из которых 
+    //обычные (31536000000), а четвертый - високосный (31622400000)
+     if (age >= 18) {
+        return true
+     } else  {
+        return false
+     } 
+    }
